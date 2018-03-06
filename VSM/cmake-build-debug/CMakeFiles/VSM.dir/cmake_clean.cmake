9b89636a34e08171c891e9af2c1ea609d871e6f1
Lars Holdijk <larsholdijk@gmail.com>
2018-03-06 14:18:26 +0100

    .

---
+++ VSM/cmake-build-debug/CMakeFiles/VSM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VSM.dir/main.c.o"
+  "VSM.pdb"
+  "VSM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/VSM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
